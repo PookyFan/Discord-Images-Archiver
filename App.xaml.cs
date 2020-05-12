40f23d4a5295528e5ebb5b91a6e72550db0a2111
PookyFan <pookyfan@kancolle.pl>
2020-05-12 12:51:43 +0200

    Changed default namespace name (just a cosmetic change).

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Discord_Images_Archiver
+namespace DiscordImagesArchiver
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Discord_Images_Archiver
+namespace DiscordImagesArchiver
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
